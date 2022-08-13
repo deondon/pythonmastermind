284feaa49c9f0b03d6d98135556d59ae11353208
Deon Don <22507662+deondon@users.noreply.github.com>
2022-08-13 11:31:40 -0400

    Created second file

---
+++ secondfile.py
@@ -0,0 +1,46 @@
+#Rebulding Typeform Bet Generator in Python
+
+#https://pavlok.typeform.com/c/O6DUy5rc
+
+print("Welcome to The Bet Generator")
+print("Press Go to Continue...")
+
+#How to create "Go" button?
+
+print("""
+Rule Updates
++100 For First Time Players Only.
+-10 Points if no Bet submitted by Deadline.
+-5 Points if No Screen Shot of Report submitted before Deadline.
+""")
+
+#points = input("How many Trust Points do you have Today?\n")
+#int(points)
+#print("You have " + points + " Trust Points")
+
+
+
+#betstatement = input(print("""
+#What is your most important outcome for tomorrow?
+#Complete this prompt: I must Spend at least 25 Minutes ______________.
+#"""))
+
+#betstatement = "I must Spend at least 25 Minutes " 
+
+#print ("I must Spend at least 25 Minutes " + betstatement)
+
+# how to Present a list of choices or prompts?
+
+#bet = input("How many Points are you Betting You won't fail?\n")
+#int(bet)
+
+#deadline = input("""Before what time today will you get it done?
+#If this is during a Supervised Pomodoro 
+#it must be within or very close to the end of the Pomodoro. 
+#If it's after it must be before the end of your work day.
+#""")
+
+#time_ns(deadline)
+
+#print("You have " + points + " Trust Points")
+#print("You Bet " + bet + " Trust Points")
