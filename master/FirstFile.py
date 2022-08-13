1e85df7259664a4070809cb957867254facf9625
Deon Don <22507662+deondon@users.noreply.github.com>
2022-08-13 19:56:26 -0400

    Added to do list

--- master/FirstFile.py
+++ master/FirstFile.py
@@ -1,9 +1,26 @@
-#Rebulding Typeform Bet Generator in Python
+#Rebuilding Typeform Bet Generator in Python
 #Why? because I cannot store variables or user profiles in Typeform
 
 #https://pavlok.typeform.com/c/O6DUy5rc
 #Long Term Goal is to execute via a sms to users phone
 #Short Team Goal is to execute via a url
+
+
+#MVP BET GEN FUNCTION TODO LIST
+
+#How to make available via url on mobile? #Django?
+#How to get before time (deadline) and store in variable?
+#How to Present a list of choices or prompts?
+#How to make User Profiles via gmail? #Django?
+#How to make available via sms? #Twilio?
+
+#DONE
+#Connect VSCode to Github - Passed
+#Download VSCode
+#Pause Spyder Usage - Connect github to Spyder - Failed
+#Connect github to VSCode
+#Upload code to github
+#Download Spyder
 
 #print("Welcome to The Bet Generator")
 #print("Press Go to Continue...")
@@ -35,8 +52,8 @@ print("Before what time will you get it done?\n")
 print("If this is during a Supervised Pomodoro")
 print("it must be within or very close to the end of the Pomodoro")
 print("If it's after it must be before the end of your work day.\n")
-deadlinetime = input("")
-print("I will get it done before " + deadlinetime + ".\n\n\n")
+beforetime = input("")
+print("I will get it done before " + beforetime + ".\n\n\n")
 #How to get time and store in variable?
 
 print("Generating your Bet Statement...")
@@ -44,4 +61,4 @@ print("Copy or Screen Shot this into your Group Chat... \n")
 
 print("I started with " + trustpoints + " Trust Points")
 print("I Bet " + betamount + " Trust Points")
-print("I must Spend at least 25 Minutes " + betstatement + " before " + deadlinetime + ".\n")
+print("I must Spend at least 25 Minutes " + betstatement + " before " + beforetime + ".\n")
